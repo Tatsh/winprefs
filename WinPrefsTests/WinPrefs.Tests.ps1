dce33f6624cdec7f1fc8768c73da5e6909cf727e
Andrew Udvare <audvare@gmail.com>
2023-10-08 04:37:01 -0400

    Start of tests

---
+++ WinPrefsTests/WinPrefs.Tests.ps1
@@ -0,0 +1,12 @@
+BeforeAll {
+  Import-Module -Name "$PSScriptRoot\..\WinPrefs" -Force
+}
+
+Describe '' {
+  Context '' {
+    BeforeEach {
+    }
+  }
+  It 'Depth cannot exceed MaxDepth' {
+  }
+}
