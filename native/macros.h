73ed6bf24d39e758a20fe89b78cae97f95ad151c
Andrew Udvare <audvare@gmail.com>
2023-11-07 02:07:44 -0500

    native/macros: add fallback for _wcsicmp

--- native/macros.h
+++ native/macros.h
@@ -1,6 +1,16 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#if !HAVE_SNWPRINTF
+#define _snwprintf swprintf
+#endif
+#if !HAVE_WCSNICMP
+#define _wcsnicmp wcsncmp
+#endif
+#if !HAVE_WCSICMP
+#define _wcsicmp wcscmp
+#endif
+
 #if !HAVE_NULLPTR_T
 //! `nullptr` definition if not defined by `stddef.h`.
 #define nullptr (void *)0
