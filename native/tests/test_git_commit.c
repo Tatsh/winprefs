d6b4fe4c30db6ee040eb1577aca8e69128ada5be
Andrew Udvare <audvare@gmail.com>
2023-11-19 10:44:30 -0500

    native/tests/test_git_commit: update

--- native/tests/test_git_commit.c
+++ native/tests/test_git_commit.c
@@ -1,19 +1,23 @@
 #include "git.h"
 
 void test_git_commit_no_git_no_fail(void **state) {
-    will_return(__wrap__wspawnlp, -1);
+    will_return(__wrap_CreateProcess, false);
     assert_true(git_commit(nullptr, nullptr));
 }
 
 void test_git_commit_no_dot_git_wgetcwd_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, INVALID_FILE_ATTRIBUTES);
     will_return(__wrap__wgetcwd, 0);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_no_dot_git_wchdir_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, INVALID_FILE_ATTRIBUTES);
     will_return(__wrap__wgetcwd, L"");
     will_return(__wrap__wchdir, 1);
@@ -21,137 +25,154 @@ void test_git_commit_no_dot_git_wchdir_fail(void **state) {
 }
 
 void test_git_commit_no_dot_git_wchdir_fail_2(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, INVALID_FILE_ATTRIBUTES);
     will_return(__wrap__wgetcwd, L"");
     will_return(__wrap__wchdir, 0);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap__wchdir, 1);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
-void test_git_commit_no_dot_git_wspawnlp_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+void test_git_commit_no_dot_git_CreateProcess_fail(void **state) {
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, INVALID_FILE_ATTRIBUTES);
     will_return(__wrap__wgetcwd, L"");
     will_return(__wrap__wchdir, 0);
-    will_return(__wrap__wspawnlp, 1);
+    will_return(__wrap_CreateProcess, false);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_add_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 1);
+    will_return(__wrap_CreateProcess, false);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_GetTimeFormat_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 0);
     assert_false(git_commit(L"output-dir", nullptr));
 
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 0);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_GetDateFormat_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
-    will_return(__wrap_GetTimeFormat, 10);
-    will_return(__wrap_GetTimeFormat, 10);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 0);
     assert_false(git_commit(L"output-dir", nullptr));
 
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
-    will_return(__wrap_GetTimeFormat, 10);
-    will_return(__wrap_GetTimeFormat, 10);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 0);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
-    will_return(__wrap__wspawnlp, 1);
+    will_return(__wrap_CreateProcess, false);
     assert_false(git_commit(L"output-dir", nullptr));
 }
 
 void test_git_commit_deploy_key_wfullpath_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap__wfullpath, 0);
     will_return(__wrap__wfullpath, 0);
     assert_false(git_commit(L"output-dir", L"key"));
 }
 
 void test_git_commit_deploy_key_config_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap__wfullpath, L"");
     will_return(__wrap__wfullpath, L"");
-    will_return(__wrap__wspawnlp, 1);
+    will_return(__wrap_CreateProcess, false);
     assert_false(git_commit(L"output-dir", L"key"));
 }
 
 void test_git_commit_deploy_key_push_fail(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap__wfullpath, L"");
     will_return(__wrap__wfullpath, L"");
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_get_git_branch, L"master");
-    will_return(__wrap__wspawnlp, 1);
+    will_return(__wrap_CreateProcess, false);
     assert_false(git_commit(L"output-dir", L"key"));
 }
 
 void test_git_commit_deploy_key(void **state) {
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
+    will_return_always(__wrap_WaitForSingleObject, 0);
+    will_return_always(__wrap_CloseHandle, true);
     will_return(__wrap_GetFileAttributes, FILE_ATTRIBUTE_DIRECTORY);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetTimeFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
     will_return(__wrap_GetDateFormat, 10);
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap__wfullpath, L"");
     will_return(__wrap__wfullpath, L"");
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     will_return(__wrap_get_git_branch, L"master");
-    will_return(__wrap__wspawnlp, 0);
+    will_return(__wrap_CreateProcess, true);
     assert_true(git_commit(L"output-dir", L"key"));
 }
 
@@ -164,10 +185,10 @@ const struct CMUnitTest git_commit_tests[] = {
     cmocka_unit_test(test_git_commit_deploy_key_push_fail),
     cmocka_unit_test(test_git_commit_deploy_key_wfullpath_fail),
     cmocka_unit_test(test_git_commit_fail),
+    cmocka_unit_test(test_git_commit_no_dot_git_CreateProcess_fail),
     cmocka_unit_test(test_git_commit_no_dot_git_wchdir_fail),
     cmocka_unit_test(test_git_commit_no_dot_git_wchdir_fail_2),
     cmocka_unit_test(test_git_commit_no_dot_git_wgetcwd_fail),
-    cmocka_unit_test(test_git_commit_no_dot_git_wspawnlp_fail),
     cmocka_unit_test(test_git_commit_no_git_no_fail),
 };
 
