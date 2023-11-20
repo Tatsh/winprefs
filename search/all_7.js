57e6c3b710893429f6e2673c91f7201ec0c38c4c
Andrew Udvare <audvare@gmail.com>
2023-11-19 19:16:18 -0500

    Update documentation to 0.3.0

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['save_5fpreferences_0',['save_preferences',['../registry_8h.html#a79781b26998a0a847977911358924db1',1,'registry.c']]],
-  ['shell_2eh_1',['shell.h',['../shell_8h.html',1,'']]]
+  ['save_5fpreferences_0',['save_preferences',['../registry_8h.html#adfa0ee4e801e6522d6e827f4b2af31b2',1,'registry.c']]],
+  ['set_5fdebug_5fprint_5fenabled_1',['set_debug_print_enabled',['../debug_8h.html#a3e294ae4b51b1d1887823ea10019b9cd',1,'debug.c']]],
+  ['shell_2eh_2',['shell.h',['../shell_8h.html',1,'']]]
 ];
