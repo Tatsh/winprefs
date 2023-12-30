aa8860839cb5f5dc54f542ab75857936eb241595
Andrew Udvare <audvare@gmail.com>
2023-12-30 06:20:15 -0500

    Update documentation to 0.3.2

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['escape_5ffor_5fbatch_0',['escape_for_batch',['../shell_8h.html#accaa6792c532f5910eda68a2cf9b61e9',1,'shell.c']]],
-  ['export_5fsingle_5fvalue_1',['export_single_value',['../registry_8h.html#a6a2892288440cad7f651f0b33aefef4d',1,'registry.c']]]
+  ['export_5fsingle_5fvalue_1',['export_single_value',['../registry_8h.html#a0d4f8e158c7edec2f537e127652699ff',1,'registry.c']]]
 ];
