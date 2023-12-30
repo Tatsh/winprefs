406dd2a8b0314a8a38d57f67b6e9b096d82ec530
Andrew Udvare <audvare@gmail.com>
2023-12-30 06:19:42 -0500

    native/shell: fix doc

--- native/shell.h
+++ native/shell.h
@@ -7,7 +7,6 @@
  \param input Input string.
  \param n_chars Length of the input string in word size.
  \return Pointer to escaped string. Must be freed on caller side.
- \ingroup A
  */
 wchar_t *escape_for_batch(const wchar_t *input, size_t n_chars);
 
