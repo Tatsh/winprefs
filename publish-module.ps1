69e66f1c414768165a048d5d28e2e654591a9574
Andrew Udvare <audvare@gmail.com>
2025-09-11 01:07:33 +0100

    publish-module: simplify

--- publish-module.ps1
+++ publish-module.ps1
@@ -3,18 +3,17 @@ if (${env:NUGET_API_KEY}.Length -eq 0) {
   return 1
 }
 $loc = $(Get-Location) -Replace "\\", "/"
-Remove-Item -Force -Recurse out
-New-Item -ItemType Directory out
-New-Item -ItemType Directory out\build
-Set-Location out\build
-cmake -G 'Visual Studio 17 2022' -A x64 "-DCMAKE_INSTALL_PREFIX=$loc/out/install" -DCMAKE_BUILD_TYPE=Release ../..
+Remove-Item -Force -Recurse build
+New-Item -ItemType Directory build
+Set-Location build
+cmake -G 'Visual Studio 17 2022' -A x64 "-DCMAKE_INSTALL_PREFIX=$loc/build/prefix" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release -v
 cmake --install . --config Release -v
-Set-Location ..\install
+Set-Location prefix
 Remove-Item -Path WinPrefs\*.deps.json
 Remove-Item -Path WinPrefs\*.lib
 Remove-Item -Path WinPrefs\*.pdb
 Remove-Item -Recurse -Path WinPrefs\ref
 Remove-Item -Recurse -Path WinPrefs\runtimes
 Publish-Module -Name .\WinPrefs -NuGetApiKey ${env:NUGET_API_KEY}
-Set-Location ..\..
+Set-Location ..\..\..
